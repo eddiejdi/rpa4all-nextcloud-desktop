10f135c5459b0b63fbdaa187c84444feddc6b66d
eddiejdi <edenilson.teixeira@rpa4all.com>
2026-03-27 16:36:16 -0300

    rpa4all: apply RPA4All branding to NEXTCLOUD.cmake
    - APPLICATION_NAME: RPA4All Files
    - APPLICATION_EXECUTABLE: rpa4all-files
    - APPLICATION_DOMAIN: rpa4all.com / APPLICATION_VENDOR: RPA4All
    - APPLICATION_SERVER_URL: https://nextcloud.rpa4all.com (enforced)
    - APPLICATION_REV_DOMAIN: com.rpa4all.files
    - APPLICATION_VIRTUALFILE_SUFFIX: rpa4all
    - LINUX_PACKAGE_SHORTNAME: rpa4all-files
    - NEXTCLOUD_BACKGROUND_COLOR: #0B6EA8 (RPA4All brand primary)
    - Disable updater URL (self-hosted)

--- NEXTCLOUD.cmake
+++ NEXTCLOUD.cmake
@@ -2,24 +2,23 @@
 # SPDX-FileCopyrightText: 2012 ownCloud GmbH
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
-# keep the application name and short name the same or different for dev and prod build
-# or some migration logic will behave differently for each build
+# RPA4All branded fork — keep APPLICATION_SHORTNAME == APPLICATION_NAME to avoid migration issues
 if(NEXTCLOUD_DEV)
-    set( APPLICATION_NAME       "NextcloudDev" )
-    set( APPLICATION_SHORTNAME  "NextcloudDev" )
-    set( APPLICATION_EXECUTABLE "nextclouddev" )
+    set( APPLICATION_NAME       "RPA4All FilesDev" )
+    set( APPLICATION_SHORTNAME  "RPA4AllFilesDev" )
+    set( APPLICATION_EXECUTABLE "rpa4all-files-dev" )
     set( APPLICATION_ICON_NAME  "Nextcloud" )
 else()
-    set( APPLICATION_NAME       "Nextcloud" )
-    set( APPLICATION_SHORTNAME  "Nextcloud" )
-    set( APPLICATION_EXECUTABLE "nextcloud" )
-    set( APPLICATION_ICON_NAME  "${APPLICATION_SHORTNAME}" )
+    set( APPLICATION_NAME       "RPA4All Files" )
+    set( APPLICATION_SHORTNAME  "RPA4AllFiles" )
+    set( APPLICATION_EXECUTABLE "rpa4all-files" )
+    set( APPLICATION_ICON_NAME  "Nextcloud" )
 endif()
 
 set( APPLICATION_CONFIG_NAME "${APPLICATION_EXECUTABLE}" )
-set( APPLICATION_DOMAIN     "nextcloud.com" )
-set( APPLICATION_VENDOR     "Nextcloud GmbH" )
-set( APPLICATION_UPDATE_URL "https://updates.nextcloud.org/client/" CACHE STRING "URL for updater" )
+set( APPLICATION_DOMAIN     "rpa4all.com" )
+set( APPLICATION_VENDOR     "RPA4All" )
+set( APPLICATION_UPDATE_URL "" CACHE STRING "URL for updater" )
 set( APPLICATION_HELP_URL   "" CACHE STRING "URL for the help menu" )
 
 if(APPLE AND APPLICATION_NAME STREQUAL "Nextcloud" AND EXISTS "${CMAKE_SOURCE_DIR}/theme/colored/Nextcloud-macOS-icon.svg")
@@ -28,15 +27,15 @@ if(APPLE AND APPLICATION_NAME STREQUAL "Nextcloud" AND EXISTS "${CMAKE_SOURCE_DI
 endif()
 
 set( APPLICATION_ICON_SET   "SVG" )
-set( APPLICATION_SERVER_URL "" CACHE STRING "URL for the server to use. If entered, the UI field will be pre-filled with it" )
-set( APPLICATION_SERVER_URL_ENFORCE ON ) # If set and APPLICATION_SERVER_URL is defined, the server can only connect to the pre-defined URL
-set( APPLICATION_REV_DOMAIN "com.nextcloud.desktopclient" )
-set( DEVELOPMENT_TEAM "NKUJUXUJ3B" CACHE STRING "Apple Development Team ID" )
-set( APPLICATION_VIRTUALFILE_SUFFIX "nextcloud" CACHE STRING "Virtual file suffix (not including the .)")
+set( APPLICATION_SERVER_URL "https://nextcloud.rpa4all.com" CACHE STRING "URL for the server to use. If entered, the UI field will be pre-filled with it" )
+set( APPLICATION_SERVER_URL_ENFORCE ON ) # Lock to RPA4All server
+set( APPLICATION_REV_DOMAIN "com.rpa4all.files" )
+set( DEVELOPMENT_TEAM "" CACHE STRING "Apple Development Team ID" )
+set( APPLICATION_VIRTUALFILE_SUFFIX "rpa4all" CACHE STRING "Virtual file suffix (not including the .)")
 set( APPLICATION_OCSP_STAPLING_ENABLED OFF )
 set( APPLICATION_FORBID_BAD_SSL OFF )
 
-set( LINUX_PACKAGE_SHORTNAME "nextcloud" )
+set( LINUX_PACKAGE_SHORTNAME "rpa4all-files" )
 set( LINUX_APPLICATION_ID "${APPLICATION_REV_DOMAIN}.${LINUX_PACKAGE_SHORTNAME}")
 
 set( THEME_CLASS            "NextcloudTheme" )
@@ -62,7 +61,7 @@ option( DO_NOT_USE_PROXY "Do not use system wide proxy, instead always do a dire
 option( WIN_DISABLE_USERNAME_PREFILL "Do not prefill the Windows user name when creating a new account" OFF )
 
 ## Theming options
-set(NEXTCLOUD_BACKGROUND_COLOR "#0082c9" CACHE STRING "Default Nextcloud background color")
+set(NEXTCLOUD_BACKGROUND_COLOR "#0B6EA8" CACHE STRING "RPA4All primary brand color")
 set( APPLICATION_WIZARD_HEADER_BACKGROUND_COLOR ${NEXTCLOUD_BACKGROUND_COLOR} CACHE STRING "Hex color of the wizard header background")
 set( APPLICATION_WIZARD_HEADER_TITLE_COLOR "#ffffff" CACHE STRING "Hex color of the text in the wizard header")
 option( APPLICATION_WIZARD_USE_CUSTOM_LOGO "Use the logo from ':/client/theme/colored/wizard_logo.(png|svg)' else the default application icon is used" ON )
